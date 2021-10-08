7888f2877fbbbdeb9a394b8d1bf13a40bbece3ff
Armando Giannetti <80115519@correiosnet.int>
2021-10-08 10:03:01 -0300

    Tests passing

--- src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,7 +1,10 @@
 package com.devsuperior.bds02.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +21,12 @@ public class EventController {
 
 	@Autowired
 	private EventService service;
+	
+	@GetMapping
+	public ResponseEntity<List<EventDTO>> findAll() {
+		List<EventDTO> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+	}
 
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,5 +1,8 @@
 package com.devsuperior.bds02.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
@@ -17,6 +21,12 @@ public class EventService {
 	@Autowired
 	private EventRepository repository;
 	
+	@Transactional(readOnly = true)
+	public List<EventDTO> findAll() {
+		List<Event> list = repository.findAll();
+		return list.stream().map(x -> new EventDTO(x)).collect(Collectors.toList());
+	}
+	
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
@@ -24,6 +34,7 @@ public class EventService {
 			entity.setName(dto.getName());
 			entity.setDate(dto.getDate());
 			entity.setUrl(dto.getUrl());
+			entity.setCity(new City(dto.getCityId(),null));
 			entity = repository.save(entity);
 			return new EventDTO(entity);
 		}
